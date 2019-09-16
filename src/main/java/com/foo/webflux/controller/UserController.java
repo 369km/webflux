8bf0fd95b994e31d484810ce041c164477676222
yfd <evof@qq.com>
2019-09-16 18:19:06 +0800

    fix

--- src/main/java/com/foo/webflux/controller/UserController.java
+++ src/main/java/com/foo/webflux/controller/UserController.java
@@ -3,7 +3,11 @@ package com.foo.webflux.controller;
 import com.foo.webflux.pojo.User;
 import com.foo.webflux.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -13,12 +17,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping
+    @PostMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Mono<User> save() {
-        return userService.save(new User(2L, "fee"));
+        return userService.save(new User(4L, "fee"));
     }
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<User> findUsers() {
         return userService.findUsers();
     }
