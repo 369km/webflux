a98b3c38791c48cf612a261692bbbfb437d2eb67
yfd <evof@qq.com>
2019-09-25 11:36:45 +0800

    port

--- src/main/java/com/foo/webflux/controller/FluxClient.java
+++ src/main/java/com/foo/webflux/controller/FluxClient.java
@@ -7,7 +7,7 @@ import reactor.core.publisher.Flux;
 
 public class FluxClient {
     public static void main(String[] args) {
-        WebClient client = WebClient.create("http://172.16.8.155:8080");
+        WebClient client = WebClient.create("http://172.16.8.155:8081");
         Flux<User> userFlux = client.get().uri("/user")
                 .accept(MediaType.APPLICATION_STREAM_JSON)
                 .retrieve()
